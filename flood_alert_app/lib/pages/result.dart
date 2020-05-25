9be88bea82de510c517edb0471afb62131e9b6c7
Shehana Iqbal <iqbalshehana@gmail.com>
2020-05-25 15:10:50 +0530

    --- Auto-Git Commit ---

--- flood_alert_app/lib/pages/result.dart
+++ flood_alert_app/lib/pages/result.dart
@@ -1,6 +1,9 @@
+import 'package:flood_alert_app/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+import 'homeScreen.dart';
 
 class Result extends StatefulWidget {
   final double longitude;
@@ -35,6 +38,7 @@ class _ResultState extends State<Result> {
     final response = await http.get(url);
     Map<String, dynamic> prediction = json.decode(response.body);
     final String height=prediction.toString();
+    print (height);
     return height;
   }
 
@@ -50,7 +54,13 @@ class _ResultState extends State<Result> {
         if (snapshot.hasData) {
           var height=snapshot.data;
           print(height);
-          return ListView(
+          return Scaffold(
+        appBar: AppBar(
+          title: Text("Flood Alert Warning Application"),
+          backgroundColor: Colors.indigo,
+        ),
+        drawer: DrawerUI(),
+        body:ListView(
         children: <Widget>[
           new Card(
                 child: new Container(
@@ -1061,11 +1071,37 @@ class _ResultState extends State<Result> {
                 ),
               ),
         ],
-      );
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.home),
+          backgroundColor: Colors.indigo,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
+          }),
+    );
         } else {
           print (snapshot.data);
           print ("failed");
-          return CircularProgressIndicator();
+          return Scaffold(
+      body: Center(
+        child:Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        /// Loader Animation Widget
+        CircularProgressIndicator(
+          valueColor: new AlwaysStoppedAnimation<Color>(
+              Colors.indigoAccent),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+        ),
+      ]
+  ) ,
+        )
+    );
         }
       }
     );
